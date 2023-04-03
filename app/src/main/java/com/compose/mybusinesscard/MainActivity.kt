4a20edca55c9c95a41fdeb03ea588548059cd328
Cosmin Linca <cosmin_linca@outlook.com>
2023-04-03 19:24:10 +0300

    Create composable methods for image profile and info section

--- app/src/main/java/com/compose/mybusinesscard/MainActivity.kt
+++ app/src/main/java/com/compose/mybusinesscard/MainActivity.kt
@@ -3,7 +3,9 @@ package com.compose.mybusinesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,9 +14,16 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.mybusinesscard.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.Image
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 
 // Entry point
 class MainActivity : ComponentActivity() {
@@ -34,18 +43,81 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBusinessCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(16.dp),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            elevation = 4.dp) {
-            
-        }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 4.dp
+        ) {
 
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider(
+                    modifier = Modifier.padding(0.dp, 4.dp),
+                    thickness = 2.dp,
+                    color = Color.LightGray
+                )
+                CreateInfoSection()
+            }
+        }
+    }
+}
+
+@Composable
+private fun CreateInfoSection() {
+    Column(
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Text(
+            text = "Cosmin L.",
+            fontWeight = FontWeight(550),
+            style = MaterialTheme.typography.h5,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "Android Developer",
+            modifier = Modifier.padding(3.dp),
+            fontWeight = FontWeight(400),
+            style = MaterialTheme.typography.subtitle1,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "cosmin@gmail.com",
+            modifier = Modifier.padding(3.dp),
+            fontWeight = FontWeight(350),
+            style = MaterialTheme.typography.subtitle2
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(4.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
